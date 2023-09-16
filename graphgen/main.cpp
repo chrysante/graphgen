dbef8c17136ff1ada3ea0c57fb463bc6cccbbd2d
chrysante <gruenlilie@protonmail.com>
2023-09-16 15:09:45 +0200

    Initial Commit

---
+++ graphgen/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  graphgen
+//
+//  Created by indigo on 16.09.23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
